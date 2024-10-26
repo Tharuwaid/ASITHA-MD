5986d46e584ba1fe42feb2f117596d4d5d7e2512
Tharuwaid <Tharushanethmina03@gmail.com>
2024-10-27 03:21:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=amYhVapI#sipYJ4J4Hp2W-J8xPxOh8Pu2mUlRy_A9jEopB88E8aE",
 MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
